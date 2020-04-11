86e1bc619066ed59d673f608e3b7c675a4057124
khnanthipstamp <stampc07@gmail.com>
2020-04-11 19:34:09 +0700

    login**change

--- login.php
+++ login.php
@@ -9,40 +9,26 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
     </head>
     <body>
-        <?php include 'header.php'; ?>
-        <?php
-        require_once './ConnectDB.php';
-        error_reporting(E_ALL ^ E_NOTICE);
-        $text = $_REQUEST['text'];
-        echo "<CENTER>" . $text . "</CENTER>";
-        ?>
-        <?php
-        require_once './ConnectDB.php';
-        error_reporting(E_ALL ^ E_NOTICE);
-        $text = $_REQUEST['text'];
-        session_start();
-        session_destroy();
-        ?>
+       <?php include 'header.php'; ?>
         <br>
         <div align="center">
             <div class="card"  style="width: 20%">
                 <div class="container">
                     <h2>Login</h2>
-                    <form ACTION="Check.php"  name="formlogin" method="POST" id="login"  class="form-horizontal">
+
+                    <form align="left" action="check.php" method='POST'>
+
                         <div class="form-group">
                             <label for="email">Email:</label>
                             <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
                         </div>
                         <div class="form-group">
-                            <label for="tel">Password:</label>
-                            <input type="tel" class="form-control" id="tel" placeholder="Enter tel" name="tel">
+                            <label for="pwd">Password:</label>
+                            <input type="password" class="form-control" id="pwd" placeholder="Enter phone number" name="tel">
+
                         </div>
-                        <div class="form-group form-check">
-                            <label class="form-check-label">
-                                <input class="form-check-input" type="checkbox" name="remember"> Remember me
-                            </label>
-                        </div>
-                        <button type="submit" class="btn btn-primary btn-block">Login</button>
+                        
+                        <button type="submit" name="submit" value="Login" class="btn btn-primary btn-block">Login</button>
                     </form>
                 </div>
             </div>
